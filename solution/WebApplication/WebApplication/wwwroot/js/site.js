31aef8844e267c77ba5c1d691f4f25b7f487d2cf
John Rampono <john.rampono@microsoft.com>
2020-12-24 11:08:45 +0800

    Started to add sidebar

--- solution/WebApplication/WebApplication/wwwroot/js/site.js
+++ solution/WebApplication/WebApplication/wwwroot/js/site.js
@@ -304,3 +304,29 @@ function DataTablesGridStart(GridOptions, PrepOptions) {
         
     });
 };
+
+//Sidebar Stuff
+$(document).ready(function () {
+
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal"
+        });
+
+        $('#dismiss, .overlay').on('click', function () {
+            // hide sidebar
+            $('#sidebar').removeClass('active');
+            // hide overlay
+            $('.overlay').removeClass('active');
+        });
+
+        $('#sidebarCollapse').on('click', function () {
+            // open sidebar
+            $('#sidebar').addClass('active');
+            // fade in the overlay
+            $('.overlay').addClass('active');
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
+
+
+});
