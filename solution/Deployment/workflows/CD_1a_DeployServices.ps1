8684637003e590f9d6d0875129e3b4ff8c3ce7c2
John Rampono <john.rampono@microsoft.com>
2021-05-21 14:33:23 +0800

    Modified Action

--- solution/Deployment/workflows/CD_1a_DeployServices.ps1
+++ solution/Deployment/workflows/CD_1a_DeployServices.ps1
@@ -1,5 +1,5 @@
 
-[Environment]::SetEnvironmentVariable("ENVIRONMENT_NAME", "development")
+#[Environment]::SetEnvironmentVariable("ENVIRONMENT_NAME", "development")
 . .\Steps\PushEnvFileIntoVariables.ps1
 ParseEnvFile("$env:ENVIRONMENT_NAME")
 Invoke-Expression -Command  ".\Steps\CD_SetResourceGroupHash.ps1"
--- solution/Deployment/workflows/CI_1a_BuildCode.ps1
+++ solution/Deployment/workflows/CI_1a_BuildCode.ps1
@@ -1,5 +1,5 @@
 
-[Environment]::SetEnvironmentVariable("ENVIRONMENT_NAME", "development")
+#[Environment]::SetEnvironmentVariable("ENVIRONMENT_NAME", "development")
 . .\Steps\PushEnvFileIntoVariables.ps1
 ParseEnvFile("$env:ENVIRONMENT_NAME")
 
