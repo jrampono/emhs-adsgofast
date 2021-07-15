76dc3330ac0a9baae85110992bc3e3622c5040e9
John Rampono <john.rampono@microsoft.com>
2021-07-15 14:39:31 +0800

    CICD Pipeline Fixes

--- solution/Deployment/workflows/CD_2b_ConfigureServices.ps1
+++ solution/Deployment/workflows/CD_2b_ConfigureServices.ps1
@@ -1,5 +1,5 @@
 
-Invoke-Expression -Command  ".\Steps\CD_SetResourceGroupHash.ps1"
+#Invoke-Expression -Command  ".\Steps\CD_SetResourceGroupHash.ps1"
 
 az config set extension.use_dynamic_install=yes_without_prompt
 
