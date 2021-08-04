80c06ec6ccfc5a1b0557e3feacf1dd5e3b8abb39
John Rampono <jrambo@Johns-Mac-mini.localdomain>
2021-08-04 12:39:36 +0800

    Fixed KeyVault login issue

--- solution/Deployment/workflows/Steps/CD_ConfigureKeyVault.ps1
+++ solution/Deployment/workflows/Steps/CD_ConfigureKeyVault.ps1
@@ -2,10 +2,18 @@ Write-Host "Configuring Key Vault"
 
 if($env:AdsOpts_CD_Services_KeyVault_Enable -eq "True")
 {
+    #Try to set user object id based on an interactive login
     $AADUserId = (az ad signed-in-user show | ConvertFrom-Json).objectId
+    if ([string]::IsNullOrEmpty($AADUserId))
+    {
+        #If Interactive user failed then use SP method.
+        $AADUserId = (az ad sp show --id (az account show | ConvertFrom-Json).user.name | ConvertFrom-Json).objectId                                                                             
+    }
+
+
     $functionkey = (az functionapp keys list -g $env:AdsOpts_CD_ResourceGroup_Name -n $env:AdsOpts_CD_Services_CoreFunctionApp_Name | ConvertFrom-Json).functionKeys.default
     Write-Host "Enabling Access to KeyVault and Adding Secrets"
-    #Set KeyVault Policy
+    #Set KeyVault Policy to allow logged in user to add key
     az keyvault set-policy --name $env:AdsOpts_CD_Services_KeyVault_Name --certificate-permissions backup create delete deleteissuers get getissuers import list listissuers managecontacts manageissuers purge recover restore setissuers update --key-permissions backup create decrypt delete encrypt get import list purge recover restore sign unwrapKey update verify wrapKey --object-id $AADUserId --resource-group $env:AdsOpts_CD_ResourceGroup_Name --secret-permissions backup delete get list purge recover restore set --storage-permissions backup delete deletesas get getsas list listsas purge recover regeneratekey restore set setsas update --subscription $env:AdsOpts_CD_ResourceGroup_Subscription
 
     #Save Function Key to KeyVault
@@ -16,5 +24,3 @@ else
 {
     Write-Host "Skipped Configuring Key Vault"
 }
-
-az ad signed-in-user show
