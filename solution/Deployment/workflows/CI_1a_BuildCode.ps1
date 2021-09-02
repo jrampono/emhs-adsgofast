92b54a555a52d8b2d7436c14ea1a09e2008b5275
Andreas Nerlich <andreas.nerlich@empired.com>
2021-09-02 14:57:31 +0800

    Reverted accidental ps1 script change

--- solution/Deployment/workflows/CI_1a_BuildCode.ps1
+++ solution/Deployment/workflows/CI_1a_BuildCode.ps1
@@ -8,11 +8,11 @@ if (([Environment]::GetEnvironmentVariable("AdsOpts_CI_Enable")) -eq "True")
     
     Invoke-Expression -Command  ".\Steps\CI_BuildFunctionApp.ps1"
 
-    # Invoke-Expression -Command  ".\Steps\CI_BuildWebApp.ps1"
+    Invoke-Expression -Command  ".\Steps\CI_BuildWebApp.ps1"
 
-    # Invoke-Expression -Command  ".\Steps\CI_BuildAdsGoFastDatabase.ps1"
+    Invoke-Expression -Command  ".\Steps\CI_BuildAdsGoFastDatabase.ps1"
 
-    # Invoke-Expression -Command  ".\Steps\CI_BuildDataFactory.ps1"
+    Invoke-Expression -Command  ".\Steps\CI_BuildDataFactory.ps1"
     
     Write-Host "Finishing CI.."
 }
